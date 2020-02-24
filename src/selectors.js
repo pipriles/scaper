13531b6670f6690a8ee4411798e7ec6944bf84b8
Oswaldo Capriles <oswald.capriles46@gmail.com>
2020-02-24 01:00:03 -0500

    Fix Autocomplete warning when selecting an option

--- src/selectors.js
+++ src/selectors.js
@@ -1,10 +1,10 @@
 /* isolate state queries from components */
 
 export const getSelector = (state, id) => {
-  return state.selectors[id]
+  return state.selectors[id] || null;
 }
 
 export const getField = (state, id) => {
-  return state.fields[id];
+  return state.fields[id] || null;
 }
 
